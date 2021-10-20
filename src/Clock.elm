299d37ebd970d11d8a0081ebd0b0429c1572f70f
Wolfgang Ginolas <wolfgang.ginolas@gwif.eu>
2021-10-20 22:23:17 +0200

    Hands seem to work

---
+++ src/Clock.elm
@@ -0,0 +1,66 @@
+module Clock exposing (viewClock)
+
+import Svg exposing (..)
+import Svg.Attributes exposing (..)
+
+
+viewClock : Int -> Int -> Int -> Svg msg
+viewClock hour minute second =
+    svg
+        [ viewBox "-10 -10 20 20" ]
+        [ secondHand second, minuteHand minute second, hourHand hour minute second ]
+
+
+secondHand second =
+    hand 10.0 0.3 (toFloat second * 6)
+
+
+minuteHand minute second =
+    let
+        minF =
+            toFloat minute
+
+        secF =
+            toFloat second
+    in
+    hand 9.0 0.7 ((minF + secF / 60) * 6)
+
+
+hourHand hour minute second =
+    let
+        hF =
+            toFloat hour
+
+        minF =
+            toFloat minute
+
+        secF =
+            toFloat second
+    in
+    hand 8.0 1.0 ((hF + (minF + secF / 60) / 60) * 15)
+
+
+hand : Float -> Float -> Float -> Svg msg
+hand len w deg =
+    let
+        halfWidth =
+            String.fromFloat (w / 2)
+
+        negHalfWidth =
+            String.fromFloat (-w / 2)
+    in
+    rect
+        [ x negHalfWidth
+        , y negHalfWidth
+        , width (String.fromFloat w)
+        , height (String.fromFloat len)
+        , rx halfWidth
+        , ry halfWidth
+        , transform (transformRotate (deg + 180))
+        ]
+        []
+
+
+transformRotate : Float -> String
+transformRotate deg =
+    "rotate(" ++ String.fromFloat deg ++ ")"
---
+++ src/Main.elm
@@ -0,0 +1,88 @@
+module Main exposing (Model, Msg(..), init, main, subscriptions, update, view)
+
+import Browser
+import Clock exposing (viewClock)
+import Html exposing (..)
+import Task
+import Time
+
+
+
+-- MAIN
+
+
+main =
+    Browser.element
+        { init = init
+        , view = view
+        , update = update
+        , subscriptions = subscriptions
+        }
+
+
+
+-- MODEL
+
+
+type alias Model =
+    { zone : Time.Zone
+    , time : Time.Posix
+    }
+
+
+init : () -> ( Model, Cmd Msg )
+init _ =
+    ( Model Time.utc (Time.millisToPosix 0)
+    , Task.perform AdjustTimeZone Time.here
+    )
+
+
+
+-- UPDATE
+
+
+type Msg
+    = Tick Time.Posix
+    | AdjustTimeZone Time.Zone
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        Tick newTime ->
+            ( { model | time = newTime }
+            , Cmd.none
+            )
+
+        AdjustTimeZone newZone ->
+            ( { model | zone = newZone }
+            , Cmd.none
+            )
+
+
+
+-- SUBSCRIPTIONS
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    Time.every 1000 Tick
+
+
+
+-- VIEW
+
+
+view : Model -> Html Msg
+view model =
+    let
+        hour =
+            Time.toHour model.zone model.time
+
+        minute =
+            Time.toMinute model.zone model.time
+
+        second =
+            Time.toSecond model.zone model.time
+    in
+    viewClock hour minute second
